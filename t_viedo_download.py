a08ae1a6b96cdfe468cdfe384927774cba235d20
Vaibhav1Patel <vaibhavPtridhyatech@gmail.com>
2021-08-26 13:51:34 +0530

    'abaa'

--- t_viedo_download.py
+++ t_viedo_download.py
@@ -2,7 +2,7 @@ from tkinter import *
 from pytube import YouTube
 
 root = Tk()
-root.geometry('500x300')
+root.geometry('800x500')
 root.resizable(0,0)
 root.title("DataFlair-youtube video downloader")
 
